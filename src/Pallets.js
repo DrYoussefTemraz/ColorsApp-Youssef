2097a788d485a77a6117243b6663dbf3ec494a21
DrYoussefTemraz <youseftahertemraz@gmail.com>
2024-01-19 11:48:46 +0200

    update

---
+++ src/Pallets.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Pallets extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pallets..</h1>
+      </div>
+    );
+  }
+}
+export default Pallets;
