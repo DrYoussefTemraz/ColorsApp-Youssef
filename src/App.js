6fb93a33a73318cff38dacb3e75a42ea6d96c67b
DrYoussefTemraz <youseftahertemraz@gmail.com>
2024-01-19 20:11:51 +0200

    non react modification, usage of chroma-js liberary and some for loops js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import "./App.css";
-import Pallete from "./Pallete";
+import React, { Component } from "react";
+import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <div>
-      <Pallete {...seedColors[4]} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+    return (
+      <div>
+        <Palette {...seedColors[4]} />
+      </div>
+    );
+  }
 }
 
 export default App;
