1bcb44c07dc15369fd8ad098bef27fc5ce3cec4a
DrYoussefTemraz <youseftahertemraz@gmail.com>
2024-01-21 12:31:22 +0200

    handlechanfe and handle format of both parent and child component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      format: "hex",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, handleChange } = this.props;
+    const { format } = this.state;
+
     return (
       <header className="Navbar">
         <div className="logo">
@@ -22,6 +38,13 @@ class Navbar extends Component {
               onChangeComplete={changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX: #ffffff </MenuItem>
+            <MenuItem value="rgb">rgb(255,255,255,255) </MenuItem>
+            <MenuItem value="rgba">rgba(255,255,255,255,0.1)</MenuItem>
+          </Select>
         </div>
       </header>
     );
