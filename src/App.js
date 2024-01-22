ad02c23b96d693faf55022c298162accd0e9e3a5
DrYoussefTemraz <youseftahertemraz@gmail.com>
2024-01-22 19:33:02 +0200

    implementation of react router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 import Palette from "./Pallete";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
+import { Route, Routes } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          Component={() => <h1>PALETTE LIST GOES HERE</h1>}
+        />
+        <Route
+          exact
+          path="/palette/:id"
+          Component={() => <h1>INDIVIDUAL PALETTE</h1>}
+        />
+      </Routes>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
