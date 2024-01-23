5c517b802d80d8338c3080731865bc1527bea891
DrYoussefTemraz <youseftahertemraz@gmail.com>
2024-01-23 15:40:11 +0200

    adding a demo to explain the withStyle and styled components liberary

---
+++ src/MiniPaletteDemo.js
@@ -0,0 +1,41 @@
+// import React from "react";
+// import { withStyles } from "@mui/styles";
+
+// const styles = {
+//   main: {
+//     backgroundColor: "purple",
+//     border: "3px solid teal",
+//     color: "red",
+//   },
+//   secondary: {
+//     backgroundColor: "pink",
+//     // it means that selected hi have this styles add &
+//     "& h1": {
+//       color: "white",
+//       "& span": {
+//         backgroundColor: "yellow",
+//       },
+//     },
+//   },
+// };
+
+// function MiniPalette(props) {
+//   const { classes } = props;
+//   console.log(classes);
+
+//   return (
+//     <div className={classes.main}>
+//       <h1>Mini Palette</h1>
+//       <section className={classes.secondary}>
+//         <h1>
+//           Mini Palette <span>ajskhd</span>
+//         </h1>
+//         {/* any thing inside main is red */}
+//         <span>Try Youssef </span>
+//       </section>
+//     </div>
+//   );
+// }
+// // add withStyle then class name then the function yu need to export
+
+// export default withStyles(styles)(MiniPalette);
