af1d663922b6ee9043bff189be77364793aa6fdf
DrYoussefTemraz <134495550+DrYoussefTemraz@users.noreply.github.com>
2024-01-23 14:15:37 +0200

    Revert "Material UI and palettes finetuning"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React, { Component } from "react";
-import Palette from "./Palette";
+import Palette from "./Pallete";
 import seedColors from "./seedColors";
-import PaletteList from "./PaletteList";
 import { generatePalette } from "./colorHelpers";
-import { Route, Routes, useParams } from "react-router-dom";
 
 class App extends Component {
-  findPalette(id) {
-    return seedColors.find((palette) => palette.id === id);
-  }
-
   render() {
-    const PaletteWrapper = () => {
-      const { id } = useParams();
-      const palette = generatePalette(this.findPalette(id));
-      return <Palette palette={palette} />;
-    };
     return (
-      <Routes>
-        <Route exact path="/" element={<PaletteList palettes={seedColors} />} />
-        <Route exact path="/palette/:id" element={<PaletteWrapper />} />
-      </Routes>
+      <div>
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
     );
   }
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,39 +1,15 @@
 import React, { Component } from "react";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import Snackbar from "@mui/material/Snackbar";
-import { Link } from "react-router-dom";
-import IconButton from "@mui/material/IconButton";
-// import CloseIcon from "@mui/material/closeIcon";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      format: "hex",
-      open: false,
-    };
-    this.handleFormatChange = this.handleFormatChange.bind(this);
-    this.closeSnackbar = this.closeSnackbar.bind(this);
-  }
-  handleFormatChange(e) {
-    this.setState({ format: e.target.value, open: true });
-    this.props.handleChange(e.target.value);
-  }
-  closeSnackbar() {
-    this.setState({ open: false });
-  }
   render() {
     const { level, changeLevel } = this.props;
-    const { format } = this.state;
-
     return (
       <header className="Navbar">
         <div className="logo">
-          <Link to="/">Youssef React Color App</Link>
+          <a href="#"> Youssef React Color App </a>
         </div>
         <div className="slider-container">
           <span>level: {level}</span>
@@ -47,36 +23,6 @@ class Navbar extends Component {
             />
           </div>
         </div>
-        <div className="select-container">
-          <Select value={format} onChange={this.handleFormatChange}>
-            <MenuItem value="hex">HEX: #eedd86 </MenuItem>
-            <MenuItem value="rgb">rgb(255,255,255,255) </MenuItem>
-            <MenuItem value="rgba">rgba(255,255,255,255,0.1)</MenuItem>
-          </Select>
-        </div>
-        <Snackbar
-          anchorOrigin={{ vertical: "buttom", horizontal: "left" }}
-          open={this.state.open}
-          autoHideDuration={3000}
-          message={
-            <span id="message-id">
-              Format changed to {format.toLocaleUpperCase()}
-            </span>
-          }
-          ContentProps={{ "aria-describedby": "message-id" }}
-          onClose={this.closeSnackbar}
-          action={[
-            <IconButton
-              color="inherit"
-              size="small"
-              key="close"
-              aria-label="close"
-              onClick={this.closeSnackbar}
-            >
-              CLOSE
-            </IconButton>,
-          ]}
-        />
       </header>
     );
   }
