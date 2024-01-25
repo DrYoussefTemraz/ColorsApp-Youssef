788883e2f5e30a0e0a1680b7b0de3a802b611976
DrYoussefTemraz <youseftahertemraz@gmail.com>
2024-01-25 22:04:05 +0200

    update

---
+++ src/singleColorPalette.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class SingleColorPalette extends Component {
+  render() {
+    return <div>SINGLE COLOR PALETTE for a component</div>;
+  }
+}
+export default SingleColorPalette;
